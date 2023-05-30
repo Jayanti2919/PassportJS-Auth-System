11e3758dc5dc6d11bfdcf648fc561eaa9d6e9bc5
Jayanti2919 <jayanti2919@gmail.com>
2023-05-30 12:40:56 +0530

    added users model

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,54 +2,47 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import * as dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import User from "../models/Users.js";
 
 const router = express.Router();
-dotenv.config()
+dotenv.config();
 
-const users = []
+const users = [];
 
-router.route('/').get((req, res) => {
-    res.json(users)
-})
+router.route("/").get((req, res) => {
+  res.json(users);
+});
 
-router.route('/').post(async(req, res) => {
-    try{
-        const salt = await bcrypt.genSalt()
-        const hashedPass = await bcrypt.hash(req.body.password, salt)
-        const user = { name: req.body.name , password: hashedPass }
-        
-        const client = new MongoClient(process.env.MONGO_URI);
-        await client.connect();
-        const collection = client.db("MernTestbyJ").collection("users");
-        collection.insertOne(user);
-        await client.close();
+router.route("/").post(async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "unable to create user" });
+  }
+});
 
-        users.push(user)
-        res.status(201).send()
-    } catch(error) {
-        console.log(error)
-        res.status(500).json({"msg": "unable to create user"})
+router.route("/login").post(async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
-})
 
-router.route('/login').post(async (req, res) => {
-    const user = users.find(user => user.name === req.body.name)
-    if(user === null) {
-        res.status(400).send("User not found")
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
     }
-    try{
-        if(await bcrypt.compare(req.body.password, user.password)) {
-            const authUser = { name: req.body.name }
-            const accessToken = jwt.sign(authUser, process.env.ACCESS_TOKEN)
-            res.status(200).json({accessToken: accessToken})
-        } else{
-            res.status(400).send("Log in rejected")
-        }
-    } catch(error) {
-        console.log(error)
-        res.status(500).send("Something went wrong")
-    }
-})
+
+    res.status(200).send("Log in successful");
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 export default router;
