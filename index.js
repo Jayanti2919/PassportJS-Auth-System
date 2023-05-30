a9f24ca69a5deb99d74bdbcdb01df5b1bbc98d31
Jayanti2919 <jayanti2919@gmail.com>
2023-05-30 09:54:29 +0530

    added create user functionality

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.use('/api/users', usersRoute);
 
 app.listen(8080, ()=>{console.log("Server started at port 8080")})
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -6,6 +7,21 @@ const users = []
 
 router.route('/').get((req, res) => {
     res.json(users)
+})
+
+router.route('/').post(async(req, res) => {
+    try{
+        const salt = await bcrypt.genSalt()
+        const hashedPass = await bcrypt.hash(req.body.password, salt)
+        const user = { name: req.body.name , password: hashedPass }
+        console.log(salt)
+        console.log(hashedPass)
+        users.push(user)
+        res.status(201).send()
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({"msg": "unable to create user"})
+    }
 })
 
 export default router;
