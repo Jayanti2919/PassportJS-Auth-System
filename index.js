fe4e7d4c2c0e648e32a662f9a5a6a1f2f46b6c78
Jayanti2919 <jayanti2919@gmail.com>
2023-05-30 09:31:27 +0530

    fixed get api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import userRoutes from "./routes/users.routes.js"
+import usersRoute from "./routes/users.routes.js"
 import express from 'express'
 
 
 const app = express()
 
-app.get('/api/users', userRoutes);
+app.use('/api/users', usersRoute);
 
 app.listen(8080, ()=>{console.log("Server started at port 8080")})
