bbb02ef3acd3cc3779824c2b6abe2dea90c400e5
Jayanti2919 <jayanti2919@gmail.com>
2023-05-30 10:07:56 +0530

    added login

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -24,4 +24,21 @@ router.route('/').post(async(req, res) => {
     }
 })
 
+router.route('/login').post(async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if(user === null) {
+        res.status(400).send("User not found")
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.status(200).send("Log in successful")
+        } else{
+            res.status(400).send("Log in rejected")
+        }
+    } catch(error) {
+        console.log(error)
+        res.status(500).send("Something went wrong")
+    }
+})
+
 export default router;
